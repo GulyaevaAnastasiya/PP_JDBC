f2514da194ff66add35c64de0f1e6c9201bcdb63
Anastasiya Gulyaeva <orb.prog@gmail.com>
2023-11-18 12:26:11 +0300

    first commit

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,35 +1,124 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
+
+    public static final String CREATE_USERS_TABLE = """
+            CREATE TABLE IF NOT EXISTS `katatest_db`.`users`(
+              `id` INT NOT NULL AUTO_INCREMENT,
+              `name` VARCHAR(45) NOT NULL ,
+              `last_name` VARCHAR(45) NOT NULL,
+              `age` INT NOT NULL,
+              PRIMARY KEY (`id`));
+            """;
+
+    public static final String DROP_USERS_TABLE = """
+            DROP TABLE IF EXISTS users
+            """;
+
+    public static final String DELETE_SQL = """
+            DELETE FROM users
+            WHERE id = ?
+            """;
+
+    public static final String SAVE_USER = """
+            INSERT INTO users (name, last_name, age)
+            VALUES (?, ?, ?);
+            """;
+
+    public static final String GET_ALL_USERS = """
+            SELECT id,
+                name,
+                last_name,
+                age
+            FROM users   
+            """;
+
+    public static final String CLEAN_USERS_TABLE = """
+            DELETE FROM users
+            """;
+
+
     public UserDaoJDBCImpl() {
 
     }
 
     public void createUsersTable() {
-
+        try (Connection connection = Util.getConnection();
+             Statement statement = connection.createStatement()) {
+            statement.executeUpdate(CREATE_USERS_TABLE);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void dropUsersTable() {
-
+        try (Connection connection = Util.getConnection();
+             Statement statement = connection.createStatement()) {
+            statement.executeUpdate(DROP_USERS_TABLE);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
+        try (Connection connection = Util.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(SAVE_USER)) {
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, lastName);
+            preparedStatement.setByte(3, age);
 
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void removeUserById(long id) {
-
+        try (Connection connection = Util.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(DELETE_SQL)) {
+            preparedStatement.setLong(1, id);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public List<User> getAllUsers() {
-        return null;
+        List<User> users = new ArrayList<>();
+
+        try (Connection connection = Util.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(GET_ALL_USERS)) {
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                String lastName = resultSet.getString("last_name");
+                byte age = resultSet.getByte("age");
+                User user = new User(name, lastName, age);
+                user.setId((long)id);
+                users.add(user);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return users;
     }
 
     public void cleanUsersTable() {
-
+        try (Connection connection = Util.getConnection();
+             Statement statement = connection.createStatement()) {
+            statement.executeUpdate(CLEAN_USERS_TABLE);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,22 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private static final String USER_NAME = "root";
+    private static final String PASSWORD = "001528";
+    private static final String CONNECTION_URL = "jdbc:mysql://localhost:3306/katatest_db";
+
+    public static Connection getConnection() {
+
+        try {
+            return DriverManager.getConnection(CONNECTION_URL, USER_NAME, PASSWORD);
+        } catch (SQLException e) {
+            System.out.println("Ошибка подключения к базе данных");
+            throw new RuntimeException(e);
+        }
+    }
 }
